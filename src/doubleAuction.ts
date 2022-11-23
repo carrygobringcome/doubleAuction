f0517a19e3ce5c7662b6ddcdbf7a5fec2164fffc
rootsoftrust <82091132+rootsoftrust@users.noreply.github.com>
2022-11-23 23:02:29 +0100

    Update doubleAuction.ts
    update how bid is created

--- src/doubleAuction.ts
+++ src/doubleAuction.ts
@@ -1,14 +1,14 @@
 import {
-  Field,
-  SmartContract,
-  method,
-  Bool,
-  state,
-  State,
-  isReady,
-  Poseidon,
-  PrivateKey,
-  PublicKey,
+    Field,
+    SmartContract,
+    method,
+    Bool,
+    state,
+    State,
+    isReady,
+    Poseidon,
+    PrivateKey,
+    PublicKey,
 } from 'snarkyjs';
 
 // check what this should be export { deploy, submitSolution, getZkAppState, createLocalBlockchain };
@@ -19,73 +19,111 @@ export { isReady };
 await isReady;
 
 export const bidders = {
-  bidder1: PrivateKey.random(),
-  bidder2: PrivateKey.random(),
-  bidder3: PrivateKey.random(),
+    bidder1: PrivateKey.random(),
+    bidder2: PrivateKey.random(),
+    bidder3: PrivateKey.random(),
 };
 
 export const askers = {
-  asker1: PrivateKey.random(),
-  asker2: PrivateKey.random(),
-  asker3: PrivateKey.random(),
+    asker1: PrivateKey.random(),
+    asker2: PrivateKey.random(),
+    asker3: PrivateKey.random(),
 };
+
+// set a start price
+
+
+// initialise bid
+let theBid = 0;
+
+// create structure for bid framework - for now use struct
+
+
+class createBid extends Struct({
+    publicKey: PublicKey,
+    bid: theBid
+}) {}
+
+
 
 
 export class doubleAuction extends SmartContract {
 
-  // make bidders, askers, their hashed prices available on-chain
+    // make bidders, askers, public keys available on-chain
 
-@state(PublicKey) registeredBidder1 = State<PublicKey>();
-@state(PublicKey) registeredBidder2 = State<PublicKey>();
-@state(PublicKey) registeredBidder3 = State<PublicKey>();
+    @state(PublicKey) registeredBidder1 = State<PublicKey>();
+    @state(PublicKey) registeredBidder2 = State<PublicKey>();
+    @state(PublicKey) registeredBidder3 = State<PublicKey>();
 
-@state(PublicKey) registeredAsker1 = State<PublicKey>();
-@state(PublicKey) registeredAsker2 = State<PublicKey>();
-@state(PublicKey) registeredAsker3 = State<PublicKey>();
-
-@state(Field) registeredBidder1Price = State<Field>();
-@state(Field) registeredBidder2Price = State<Field>();
-@state(Field) registeredBidder3Price = State<Field>();
-
-@state(Field) registeredAsker1Price = State<Field>();
-@state(Field) registeredAsker2Price = State<Field>();
-@state(Field) registeredAsker3Price = State<Field>();
+    @state(PublicKey) registeredAsker1 = State<PublicKey>();
+    @state(PublicKey) registeredAsker2 = State<PublicKey>();
+    @state(PublicKey) registeredAsker3 = State<PublicKey>();
 
 
-@method init() {
-    // define initial state
-    // how would check of registration work?
-    this.registeredBidder1.set(bidders['bidder1'].toPublicKey());
-    this.registeredBidder2.set(bidders['bidder2'].toPublicKey());
-    this.registeredBidder3.set(bidders['bidder3'].toPublicKey());
+    @state(Field) registeredAsker1Price = State<Field>();
+    @state(Field) registeredAsker2Price = State<Field>();
+    @state(Field) registeredAsker3Price = State<Field>();
+    @state(Field) flashPrice = State<Field>();
 
-    this.registeredBidder1.set(bidders['bidder1'].toPublicKey());
-    this.registeredBidder2.set(bidders['bidder2'].toPublicKey());
-    this.registeredBidder3.set(bidders['bidder3'].toPublicKey());
+    @method init() {
+        // define initial state
+        // define flash price
+        this.registeredBidder1.set(bidders['bidder1'].toPublicKey());
+        this.registeredBidder2.set(bidders['bidder2'].toPublicKey());
+        this.registeredBidder3.set(bidders['bidder3'].toPublicKey());
 
-    // this would have to be input via the ui - start here, then console then ui
-    // default bid ask for example
- // hash offchain
- //   this.registeredBidder1Price.set(Poseidon.hash(Bid1));
+        this.registeredBidder1.set(bidders['bidder1'].toPublicKey());
+        this.registeredBidder2.set(bidders['bidder2'].toPublicKey());
+        this.registeredBidder3.set(bidders['bidder3'].toPublicKey());
+
+        // generate flash price
+        // this would have to be input via the ui - start here, then console then ui
+        // default bid ask for example
+        // hash offchain
+        //   this.registeredBidder1Price.set(Poseidon.hash(Bid1));
 
 
-    //  this.user2.set(users['SuperBob'].toPublicKey());
-    //  this.user3.set(users['MegaBob'].toPublicKey());
-    // this.message.set(Field(0));
-    //  this.messageHistoryHash.set(Field(0));
+        //  this.user2.set(users['SuperBob'].toPublicKey());
+        //  this.user3.set(users['MegaBob'].toPublicKey());
+        // this.message.set(Field(0));
+        //  this.messageHistoryHash.set(Field(0));
 
-    // generate ask and bids?
-}
+        // generate ask and bids?
+    }
 
 
 
 // the method should publish proves - last 8 bytes of hashes of bid, public key, and ID to prove meter is pro or consumer
-@method publishBid (bid: Field, signerPrivateKey: PrivateKey)  {
+// bidder checks flash price, and hashes the percentage they are prepared to pay
+    @method publishBid (bid: Field, potentialBidderPrivateKey: PrivateKey)  {
+
+// check if bidder is allowed to bid
+        potentialBidderPublicKey = potentialBidderPrivateKey.toPublicKey();
+
+// Get approved public keys
+        const bidder1 = this.bidder1.get();
+        const bidder2 = this.bidder2.get();
+        const bidder3 = this.bidder3.get();
+
+// Assert that signerPublicKey is one of the approved public keys
+        potentialBidderPublicKey
+            .equals(user1)
+            .or(potentialBidderPublicKey.equals(bidder2))
+            .or(potentialBidderPublicKey.equals(bidder3))
+            .assertEquals(true);
+// hash bid and create bid
+        const userBid = Poseidon.hash(bid);
+        createBid
 
 
-}
+    }
 
-@method publishAsk (ask: Field, signerPrivateKey: PrivateKey)  {
+@method askerCheckBid {
+
+        // get bids from on-chain
+
+
+
 
 }
 
